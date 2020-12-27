101018ca0c8d9d0859d120776331cf7f5583f082
Mumtoz Kodirov <imumtoz@gmail.com>
2020-12-27 10:20:31 +0500

    Delete duplications

--- creational/adapter/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Composer autoload
-require 'vendor/autoload.php';
-
-// Boot a file cache
-(new App\Cache)->boot( new App\File );
-
-// Boot a redis cache
-(new App\Cache)->boot( new App\Adapters\RedisAdapter(new App\Redis) );
